e8afb171319dfbaaf7425f43a3ace37ebff5b1cd
Ryan Peden <rpeden@users.noreply.github.com>
2023-12-23 22:17:41 -0500

    Updating existing function call test and added a test that checks enum value function calls

--- src/test/java/io/kestra/plugin/openai/ChatCompletionTest.java
+++ src/test/java/io/kestra/plugin/openai/ChatCompletionTest.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.theokanning.openai.completion.chat.ChatFunctionCall;
-import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.*;
 import com.theokanning.openai.service.OpenAiService;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
@@ -14,6 +13,7 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -38,7 +38,7 @@ public class ChatCompletionTest {
 
         ChatCompletion task = ChatCompletion.builder()
             .apiKey(this.apiKey)
-            .model("gpt-3.5-turbo-0613")
+            .model("gpt-3.5-turbo")
             .messages(messages)
             .build();
 
@@ -55,7 +55,7 @@ public class ChatCompletionTest {
 
         ChatCompletion task = ChatCompletion.builder()
             .apiKey(this.apiKey)
-            .model("gpt-3.5-turbo-0613")
+            .model("gpt-3.5-turbo")
             .prompt("what is the capital of France?")
             .build();
 
@@ -90,38 +90,31 @@ public class ChatCompletionTest {
     }
 
     @Test
-    void runFunctionWithMessage() throws Exception {
+    void runFunction() throws Exception {
         RunContext runContext = runContextFactory.of();
 
-        String openAiFunctionSchema = "{\n" +
-            "  \"type\": \"object\",\n" +
-            "  \"properties\": {\n" +
-            "    \"location\": {\n" +
-            "      \"type\": \"string\",\n" +
-            "      \"description\": \"The city and state/province, and country, e.g. San Francisco, CA, USA\"\n" +
-            "    },\n" +
-            "    \"unit\": {\n" +
-            "      \"type\": \"string\",\n" +
-            "      \"description\": \"The temperature unit this city uses.\",\n" +
-            "      \"enum\": [\n" +
-            "        \"celsius\",\n" +
-            "        \"fahrenheit\"\n" +
-            "      ]\n" +
-            "    }\n" +
-            "  },\n" +
-            "  \"required\": [\n" +
-            "    \"location\",\n" +
-            "    \"unit\"\n" +
-            "  ]\n" +
-            "}";
-
-        ObjectMapper mapper = OpenAiService.defaultObjectMapper();
-
-        JsonParser jp = mapper.getFactory().createParser(openAiFunctionSchema);
-        JsonNode parameters = mapper.readTree(jp);
+        List<ChatCompletion.PluginChatFunctionParameter> parameters = List.of(
+            ChatCompletion.PluginChatFunctionParameter.builder()
+                .name("location")
+                .type("string")
+                .description("The city and state/province, and country, e.g. San Francisco, CA, USA")
+                .required(true)
+                .build(),
+            ChatCompletion.PluginChatFunctionParameter.builder()
+                .name("unit")
+                .type("string")
+                .description("The temperature unit this city uses.")
+                .required(true)
+                .build()
+        );
 
         List<ChatCompletion.PluginChatFunction> functions = List.of(
-            new ChatCompletion.PluginChatFunction("test", "finds the most relevant city and its temperature unit", parameters)
+            ChatCompletion.PluginChatFunction.builder()
+                .name("test")
+                .description("finds the most relevant city and its temperature unit")
+                .parameters(parameters)
+                .build()
+
         );
 
         List<ChatMessage> messages = List.of(
@@ -131,20 +124,74 @@ public class ChatCompletionTest {
         ChatCompletion task = ChatCompletion.builder()
             .apiKey(this.apiKey)
             .model("gpt-3.5-turbo-0613")
-            .apiTimeout(30)
             .messages(messages)
             .functions(functions)
-            .functionCall("test")
+            .functionCall("auto")
             .build();
 
         ChatCompletion.Output runOutput = task.run(runContext);
-
-        assertThat(runOutput.getChoices().get(0).getFinishReason(), containsString("function_call"));
-
         ChatFunctionCall functionCall = runOutput.getChoices().get(0).getMessage().getFunctionCall();
+
         assertThat(functionCall.getName(), containsString("test"));
         assertThat(functionCall.getArguments().get("location").toString(), containsString("Lyon"));
+        assertThat(runOutput.getModel(), containsString("gpt-3.5-turbo"));
+    }
 
+    @Test
+    void runFunctionWithEnumValues() throws Exception {
+        RunContext runContext = runContextFactory.of();
+
+        List<ChatCompletion.PluginChatFunctionParameter> parameters = List.of(
+            ChatCompletion.PluginChatFunctionParameter.builder()
+                .name("rating")
+                .type("string")
+                .description("A rating of what the customer thought of our restaurant based on the review they wrote.")
+                .required(true)
+                .enumValues(List.of("excellent", "acceptable", "terrible"))
+                .build(),
+            ChatCompletion.PluginChatFunctionParameter.builder()
+                .name("food_eaten")
+                .type("string")
+                .description("A list of the food the customer ate, or 'Unknown' if they did not specify.")
+                .required(true)
+                .build(),
+            ChatCompletion.PluginChatFunctionParameter.builder()
+                .name("customer_name")
+                .type("string")
+                .description("The customer's name.")
+                .required(true)
+                .build()
+        );
+
+        List<ChatCompletion.PluginChatFunction> functions = List.of(
+            ChatCompletion.PluginChatFunction.builder()
+                .name("record_customer_rating")
+                .description("Saves a customer's rating of our restaurant based on what they wrote in an online review.")
+                .parameters(parameters)
+                .build()
+        );
+
+        List<ChatMessage> messages = List.of(
+            buildMessage("user","My name is John Smith. I ate at your restaurant last week and order the steak. It was the worst steak I've even eaten. I will not be returning!")
+        );
+
+        ChatCompletion task = ChatCompletion.builder()
+            .apiKey(this.apiKey)
+            .model("gpt-3.5-turbo")
+            .messages(messages)
+            .functions(functions)
+            .functionCall("record_customer_rating")
+            .build();
+
+        ChatCompletion.Output runOutput = task.run(runContext);
+        ChatFunctionCall functionCall = runOutput.getChoices().get(0).getMessage().getFunctionCall();
+
+        assertThat(functionCall.getName(), containsString("record_customer_rating"));
+        assertThat(functionCall.getArguments().get("rating").toString(), containsString("terrible"));
+        assertThat(functionCall.getArguments()
+            .get("food_eaten").toString()
+            .toLowerCase(), containsString("steak"));
+        assertThat(functionCall.getArguments().get("customer_name").toString(), containsString("John Smith"));
         assertThat(runOutput.getModel(), containsString("gpt-3.5-turbo"));
     }
 
