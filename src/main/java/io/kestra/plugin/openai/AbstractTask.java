b8189593caef37e79cd96ae8586b1d893a4d243e
Ryan Peden <rpeden@users.noreply.github.com>
2023-12-23 22:13:26 -0500

    Revert changes to `AbstractTask`

--- src/main/java/io/kestra/plugin/openai/AbstractTask.java
+++ src/main/java/io/kestra/plugin/openai/AbstractTask.java
@@ -1,23 +1,11 @@
 package io.kestra.plugin.openai;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.theokanning.openai.client.OpenAiApi;
 import com.theokanning.openai.service.OpenAiService;
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
-import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.tasks.Task;
 import io.kestra.core.runners.RunContext;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
-import okhttp3.OkHttpClient;
-import retrofit2.Retrofit;
 
 import java.time.Duration;
 
@@ -31,34 +19,12 @@ public abstract class AbstractTask extends Task implements OpenAiInterface{
 
     protected String user;
 
-    @Schema(
-        title = "The timeout value to send to the OpenAI API."
-    )
-    @PluginProperty
-    private Integer apiTimeout;
+    @Builder.Default
+    protected long clientTimeout = 10;
 
     protected OpenAiService client(RunContext runContext) throws IllegalVariableEvaluationException {
         String apiKey = runContext.render(this.apiKey);
 
-        return createOpenAiService(apiKey, Duration.ofSeconds(apiTimeout));
-    }
-
-    private static OpenAiService createOpenAiService(String token, final Duration timeout) {
-        ObjectMapper mapper = OpenAiService.defaultObjectMapper();
-        mapper.addMixIn(ChatCompletion.PluginChatFunction.class, KestraChatFunctionMixin.class);
-
-        OkHttpClient client = OpenAiService.defaultClient(token, timeout);
-        Retrofit retrofit = OpenAiService.defaultRetrofit(client, mapper);
-
-        OpenAiApi api = retrofit.create(OpenAiApi.class);
-        return new OpenAiService(api);
-    }
-
-    public abstract static class KestraChatFunctionMixin {
-        @JsonIgnore
-        abstract Class<?> getParametersClass();
-
-        @JsonProperty("parameters")
-        abstract ArrayNode getParameters();
+        return new OpenAiService(apiKey, Duration.ofSeconds(clientTimeout));
     }
 }
