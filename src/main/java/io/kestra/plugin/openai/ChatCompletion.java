bfc441300951899eb840efc7ee322eb2f93ce7aa
Loïc Mathieu <loikeseke@gmail.com>
2024-01-03 16:56:49 +0100

    feat: Micronaut 4

--- src/main/java/io/kestra/plugin/openai/ChatCompletion.java
+++ src/main/java/io/kestra/plugin/openai/ChatCompletion.java
@@ -13,7 +13,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
