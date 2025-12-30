fbee91cdbb6e6bcbae3052d531577ffc001ca598
ben8t <pimpaudben@gmail.com>
2025-12-30 15:40:48 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/openai/package-info.java
+++ src/main/java/io/kestra/plugin/openai/package-info.java
@@ -1,4 +1,6 @@
-@PluginSubGroup(title = "OpenAPI group of plugins", categories = PluginSubGroup.PluginCategory.AI)
+@PluginSubGroup(title = "OpenAPI group of plugins", categories = PluginSubGroup.PluginCategory.AI,
+    categories = { PluginSubGroup.PluginCategory.AI }
+)
 package io.kestra.plugin.openai;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
