d9b6bc9c85c29843a0c675d6b3176ae7824c466e
qp-st <quirin.parstorfer@star-trac.de>
2026-03-05 09:36:08 +0100

    feat(responses): support prompt id with variables (#122)
    * feat(responses): prompt id and prompt variables
    
    Implement Prompt ID support with Prompt Variables
    Update Docs and Metadata
    Add Prompt ID and MCP Example for Responses Task
    
    * fix(test): fix functionTool test values

--- src/test/java/io/kestra/plugin/openai/AbstractOpenAITest.java
+++ src/test/java/io/kestra/plugin/openai/AbstractOpenAITest.java
@@ -6,6 +6,28 @@ import org.assertj.core.util.Strings;
 @KestraTest
 public class AbstractOpenAITest {
     private static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
+    /* Create the following Prompt for testing:
+    <system>
+    Respond with the uppercased text that was provided by the user.
+    Example:
+    - User: qwer!
+    - Assistant: QWER!
+    </system>
+     */
+    private static final String PROMPT_ID_TO_UPPER = System.getenv("OPENAI_PROMPT_ID_TO_UPPER");
+    /* Create the following Prompt for testing:
+    <system>
+    Respond with the uppercased text that was provided by the user,
+    appending the user-provided suffix to the output (see below).
+    Example:
+    - User: qwer (provided suffix: ty)
+    - Assistant: QWERty
+    </system>
+    <user>
+    Provided suffix: {{suffix}}
+    </user>
+     */
+    private static final String PROMPT_ID_TO_UPPER_SUFFIX = System.getenv("OPENAI_PROMPT_ID_TO_UPPER_SUFFIX");
 
     protected static boolean canNotBeEnabled() {
         return Strings.isNullOrEmpty(getApiKey());
@@ -13,5 +35,21 @@ public class AbstractOpenAITest {
 
     protected static String getApiKey() {
         return OPENAI_API_KEY;
+    }
+
+    protected static boolean canNotTestPromptId() {
+        return Strings.isNullOrEmpty(getPromptIdToUpper());
+    }
+
+    protected static String getPromptIdToUpper() {
+        return PROMPT_ID_TO_UPPER;
+    }
+
+    protected static boolean canNotTestPromptIdVariables() {
+        return Strings.isNullOrEmpty(getPromptIdToUpperSuffix());
+    }
+
+    protected static String getPromptIdToUpperSuffix() {
+        return PROMPT_ID_TO_UPPER_SUFFIX;
     }
 }
